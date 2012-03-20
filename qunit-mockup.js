646ed379d58536bb28727c1c0ab664ccc6e1d861
Rob Moorman <rob@moori.nl>
2012-03-20 16:31:17 +0100

    added initial testMockup method

---
+++ qunit-mockup.js
@@ -0,0 +1,25 @@
+testMockup = function( mockupUrl, testName, expected, callback ) {
+	
+	if( arguments.length === 3 ) {
+		callback = expected;
+		expected = null;
+	}
+	
+	QUnit.test( testName, expected, function() {
+		$.ajax({
+			url: mockupUrl,
+			dataType: "html",
+			error: function() {
+				throw "Not able to load mockup data at " + mockupUrl + "!";
+			},
+			success: function( data ) {
+				$( "div#qunit-fixture" ).append( data );
+				
+				callback.apply( null, [] );
+				
+				QUnit.start();	
+			}
+		});
+	}, true );
+	
+};
